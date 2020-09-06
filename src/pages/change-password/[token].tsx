55ed298cf2a2aca3bf581851639c93df29b2853e
Youssef Samih <youssef.samih@4d.com>
2020-09-06 18:45:00 +0100

    feat: until forward tuto

--- src/pages/change-password/[token].tsx
+++ src/pages/change-password/[token].tsx
@@ -1,25 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { NextPage } from "next";
 import { Wrapper } from "../../components/Wrapper";
 import { Formik, Form } from "formik";
-import login from "../login";
 import { toErrorMap } from "../../utils/toErrorMap";
 import { InputField } from "../../components/InputField";
-import { Box, Button } from "@chakra-ui/core";
+import { Box, Button, Link, Flex } from "@chakra-ui/core";
+import { useChangePasswordMutation } from "../../generated/graphql";
+import { useRouter } from "next/router";
+import { createUrqlClient } from './../../utils/createUrqlClient';
+import { withUrqlClient } from "next-urql";
+import NextLink from "next/link";
 
 const ChangePassword: NextPage<{ token: string }> = ({ token }) => {
+  const [, changePassword] = useChangePasswordMutation();
+  const router = useRouter();
+  const [tokenError, setTokenError] = useState('');
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ newPassword: '' }}
         onSubmit={async (values, { setErrors }) => {
-        //   const response = await login(values);
-        //   if (response.data?.login.errors) {
-        //     // [{field: 'username', message: 'something wrong'}]
-        //     setErrors(toErrorMap(response.data.login.errors));
-        //   } else if (response.data?.login.user) {
-        //     router.push("/");
-        //   }
+          const response = await changePassword({token, newPassword: values.newPassword});
+          if (response.data?.changePassword.errors) {
+            // [{field: 'username', message: 'something wrong'}]
+            const errorMap = toErrorMap(response.data.changePassword.errors);
+            if ('token' in errorMap) {
+              setTokenError(errorMap.token);
+            }
+            setErrors(errorMap);
+            console.log(errorMap);
+          } else if (response.data?.changePassword.user) {
+            router.push("/");
+          }
         }}
       >
         {({ isSubmitting }) => (
@@ -30,6 +42,14 @@ const ChangePassword: NextPage<{ token: string }> = ({ token }) => {
               label="New Password"
               type="password"
             />
+            { tokenError ? (
+              <Flex>
+                <Box mr={2} style={{ color: 'red' }}>{tokenError}</Box>
+                <NextLink href="/forgot-password">
+                  <Link>Click here to get a new one</Link>
+                </NextLink>
+              </Flex>
+            ) : null }
             <Button
               mt={4}
               type="submit"
@@ -51,4 +71,4 @@ ChangePassword.getInitialProps = ({ query }) => {
   };
 };
 
-export default ChangePassword;
+export default withUrqlClient(createUrqlClient)(ChangePassword);
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Formik, Form } from 'formik';
-import { Box, Button } from '@chakra-ui/core';
+import { Box, Button, Link, Flex } from '@chakra-ui/core';
 import { Wrapper } from '../components/Wrapper';
 import { InputField } from '../components/InputField';
 import { useLoginMutation } from '../generated/graphql';
@@ -8,6 +8,7 @@ import { toErrorMap } from '../utils/toErrorMap';
 import { useRouter } from 'next/router';
 import { createUrqlClient } from '../utils/createUrqlClient';
 import { withUrqlClient } from 'next-urql';
+import NextLink from "next/link";
 
 const Login: React.FC<{}> = ({}) => {
   const router = useRouter();
@@ -43,6 +44,11 @@ const Login: React.FC<{}> = ({}) => {
                 type="password"
               />
             </Box>
+            <Flex mr={2}>
+              <NextLink href="/forgot-password">
+                <Link ml="auto">forgot password ?</Link>
+              </NextLink>
+            </Flex>
             <Button
               mt={4}
               type="submit"
